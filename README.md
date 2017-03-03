e9dc8fac5d8913469e4f27fc58c83a17b40791c9
Chuancong Gao <chuanconggao@users.noreply.github.com>
2017-03-03 10:59:18 -0800

    Add support for top-k algorithm

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Just replace the variable `db` with your own sequences, and variable `minsup` wi
 
 # Features
 Based on state-of-the-art [PrefixSpan](http://www.cs.sfu.ca/~jpei/publications/span.pdf) algorithm.
+Mining top-k patterns is also supported.
 
 # Tip
 I strongly encourage using PyPy instead of CPython to run the script for best performance. In my own experience, it is 9x times faster in average.
--- prefixspan.py
+++ prefixspan.py
@@ -1,30 +1,22 @@
 #! /usr/bin/env python
 
+"""
+Usage:
+    prefixspan.py (frequent | top-k) <threshold>
+"""
+
 from __future__ import print_function
 
 import sys
 from collections import defaultdict
+from heapq import heappop, heappush
 
-#  db = [
-    #  [0, 1, 2, 3, 4],
-    #  [1, 1, 1, 3, 4],
-    #  [2, 1, 2, 2, 0],
-    #  [1, 1, 1, 2, 2],
-#  ]
-
-#  minsup = 2
-
-db = [
-    [int(v) for v in line.rstrip().split(' ')]
-    for line in sys.stdin
-]
-
-minsup = int(sys.argv[1])
+from docopt import docopt
 
 results = []
 
-def mine_rec(patt, mdb):
-    results.append((patt, len(mdb)))
+def frequent_rec(patt, mdb):
+    results.append((len(mdb), patt))
 
     occurs = defaultdict(list)
     for (i, startpos) in mdb:
@@ -36,8 +28,52 @@ def mine_rec(patt, mdb):
 
     for (c, newmdb) in occurs.iteritems():
         if len(newmdb) >= minsup:
-            mine_rec(patt + [c], newmdb)
+            frequent_rec(patt + [c], newmdb)
 
-mine_rec([], [(i, 0) for i in xrange(len(db))])
+def topk_rec(patt, mdb):
+    heappush(results, (len(mdb), patt))
+    if len(results) > k:
+        heappop(results)
 
-print(results)
+    occurs = defaultdict(list)
+    for (i, startpos) in mdb:
+        seq = db[i]
+        for j in xrange(startpos, len(seq)):
+            l = occurs[seq[j]]
+            if len(l) == 0 or l[-1][0] != i:
+                l.append((i, j + 1))
+
+    for (c, newmdb) in sorted(occurs.iteritems(), key=(lambda (c, newmdb): len(newmdb)), reverse=True):
+        if len(results) == k and len(newmdb) <= results[0][0]:
+            break
+
+        topk_rec(patt + [c], newmdb)
+
+if __name__ == "__main__":
+    argv = docopt(__doc__)
+
+    # db = [
+        # [int(v) for v in line.rstrip().split(' ')]
+        # for line in sys.stdin
+    # ]
+
+    db = [
+        [0, 1, 2, 3, 4],
+        [1, 1, 1, 3, 4],
+        [2, 1, 2, 2, 0],
+        [1, 1, 1, 2, 2],
+    ]
+
+    if argv["frequent"]:
+        minsup = int(argv["<threshold>"])
+        f = frequent_rec
+    elif argv["top-k"]:
+        k = int(argv["<threshold>"])
+        f = topk_rec
+
+    f([], [(i, 0) for i in xrange(len(db))])
+
+    if argv["top-k"]:
+        results.sort(key=(lambda (freq, patt): (-freq, patt)))
+    for (freq, patt) in results:
+        print("{}: {}".format(patt, freq))

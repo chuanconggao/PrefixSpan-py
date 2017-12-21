2f24839bc3f5a75003c7da5d1635df89173ca12e
Chuancong Gao <chuanconggao@users.noreply.github.com>
2017-12-21 03:36:29 -0800

    Add pattern length constraint

--- prefixspan.py
+++ prefixspan.py
@@ -2,7 +2,7 @@
 
 """
 Usage:
-    prefixspan.py (frequent | top-k) <threshold>
+    prefixspan.py (frequent | top-k) <threshold> [<file>] [--minlen=1] [--maxlen=maxint]
 """
 
 from __future__ import print_function
@@ -13,11 +13,11 @@ from heapq import heappop, heappush
 
 from docopt import docopt
 
+__minlen, __maxlen = 1, sys.maxint
+
 results = []
 
-def frequent_rec(patt, mdb):
-    results.append((len(mdb), patt))
-
+def __scan(patt, mdb):
     occurs = defaultdict(list)
     for (i, startpos) in mdb:
         seq = db[i]
@@ -26,50 +26,56 @@ def frequent_rec(patt, mdb):
             if len(l) == 0 or l[-1][0] != i:
                 l.append((i, j + 1))
 
-    for (c, newmdb) in occurs.iteritems():
+    return occurs
+
+
+def frequent_rec(patt, mdb):
+    if len(patt) >= __minlen:
+        results.append((len(mdb), patt))
+
+        if len(patt) == __maxlen:
+            return
+
+    for (c, newmdb) in __scan(patt, mdb).iteritems():
         if len(newmdb) >= minsup:
             frequent_rec(patt + [c], newmdb)
 
 
 def topk_rec(patt, mdb):
-    heappush(results, (len(mdb), patt))
-    if len(results) > k:
-        heappop(results)
+    if len(patt) >= __minlen:
+        heappush(results, (len(mdb), patt))
+        if len(results) > k:
+            heappop(results)
 
-    occurs = defaultdict(list)
-    for (i, startpos) in mdb:
-        seq = db[i]
-        for j in xrange(startpos, len(seq)):
-            l = occurs[seq[j]]
-            if len(l) == 0 or l[-1][0] != i:
-                l.append((i, j + 1))
+        if len(patt) == __maxlen:
+            return
 
     for (c, newmdb) in sorted(
-            occurs.iteritems(),
+            __scan(patt, mdb).iteritems(),
             key=(lambda (c, newmdb): len(newmdb)),
             reverse=True
         ):
-        if len(results) == k and len(newmdb) <= results[0][0]:
+        newpatt = patt + [c]
+        if len(results) == k and (len(newmdb), newpatt) <= results[0]:
             break
 
-        topk_rec(patt + [c], newmdb)
+        topk_rec(newpatt, newmdb)
 
 
 if __name__ == "__main__":
-    def checkThreshold(cond):
-        threshold = int(argv["<threshold>"])
+    def checkArg(arg, cond):
+        threshold = int(argv[arg])
         if cond(threshold):
             return threshold
 
-        print("ERROR: <threshold> is not in correct range.", file=sys.stderr)
+        print("ERROR: {} is not in correct range.".format(arg), file=sys.stderr)
         sys.exit(1)
-
 
     argv = docopt(__doc__)
 
     db = [
         [int(v) for v in line.rstrip().split(' ')]
-        for line in sys.stdin
+        for line in (open(argv["<file>"]) if argv["<file>"] else sys.stdin)
     ]
     # db = [
         # [0, 1, 2, 3, 4],
@@ -79,11 +85,16 @@ if __name__ == "__main__":
     # ]
 
     if argv["frequent"]:
-        minsup = checkThreshold(lambda v: 0 < v <= len(db))
+        minsup = checkArg("<threshold>", lambda v: 0 < v <= len(db))
         func = frequent_rec
     elif argv["top-k"]:
-        k = checkThreshold(lambda v: 0 < v)
+        k = checkArg("<threshold>", lambda v: 0 < v)
         func = topk_rec
+
+    if argv["--minlen"]:
+        __minlen = checkArg("--minlen", lambda v: 0 < v)
+    if argv["--maxlen"]:
+        __maxlen = checkArg("--maxlen", lambda v: __minlen <= v)
 
     func([], [(i, 0) for i in xrange(len(db))])
 

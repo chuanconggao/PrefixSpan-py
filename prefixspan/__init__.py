deb24c6df3ccb57fe49a2b6eaadfbffd6ce63dd1
Chuancong Gao <chuanconggao@users.noreply.github.com>
2018-04-28 18:32:43 -0700

    Code cleanup

--- prefixspan/__init__.py
+++ prefixspan/__init__.py
@@ -1,3 +1,9 @@
 #! /usr/bin/env python3
 
-from .api import PrefixSpan
+from .prefixspan import PrefixSpan
+
+from .frequent import PrefixSpan_frequent
+from .topk import PrefixSpan_topk
+
+PrefixSpan.frequent = PrefixSpan_frequent
+PrefixSpan.topk = PrefixSpan_topk
--- prefixspan/api.py
+++ prefixspan/api.py
@@ -1,9 +1,1 @@
-#! /usr/bin/env python3
-
-from .prefixspan import PrefixSpan
-
-from .frequent import PrefixSpan_frequent
-from .topk import PrefixSpan_topk
-
-PrefixSpan.frequent = PrefixSpan_frequent
-PrefixSpan.topk = PrefixSpan_topk
+__init__.py
